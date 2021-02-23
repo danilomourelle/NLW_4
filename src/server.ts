1874d721d609020caffd300009f796cf2410b34f
DaniloMourelle <danilo.mourelle@icarotech.com>
2021-02-23 20:03:30 -0300

    Dia 1

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: "Pura enrolação" })
+})
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+})
+
